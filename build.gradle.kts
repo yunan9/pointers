da29a723f4290ee22af0cc9b814cfbd3aa45e777
yunan9 <itzcollor@gmail.com>
2025-11-21 21:25:02 -0500

    chore: refactor code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    id("me.champeau.jmh") version "0.7.3"
 }
 
 repositories {
@@ -10,4 +11,9 @@ repositories {
 dependencies {
     api("io.github.yunan9:commons:0.1.3-SNAPSHOT")
     api("org.jetbrains:annotations:26.0.2-1")
+
+    jmh(parseJmhDependencyAnnotation("jmh-core"))
+    jmhAnnotationProcessor(parseJmhDependencyAnnotation("jmh-generator-annprocess"))
 }
+
+fun parseJmhDependencyAnnotation(artifactId: String) = "org.openjdk.jmh:$artifactId:1.37"
