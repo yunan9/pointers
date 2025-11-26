14c11db745d5cc3ae3a1f8a84b4e36f4cd86591a
yunan9 <itzcollor@gmail.com>
2025-11-26 16:35:22 -0500

    chore: refactor code

--- src/main/java/io/github/yunan9/pointer/key/PointerKeyImpl.java
+++ src/main/java/io/github/yunan9/pointer/key/PointerKeyImpl.java
@@ -4,33 +4,16 @@ import org.jetbrains.annotations.NotNull;
 
 record PointerKeyImpl<T>(String reference, Class<T> type) implements PointerKey<T> {
 
-  PointerKeyImpl(final @NotNull String reference, final @NotNull Class<T> type) {
-    this.reference = reference;
-
-    this.type = type;
-  }
-
-  @Override
-  public @NotNull String reference() {
-    return this.reference;
-  }
-
-  @Override
-  public @NotNull Class<T> type() {
-    return this.type;
-  }
-
   @Override
   public boolean equals(final @NotNull Object o) {
     if (this == o) {
       return true;
     }
 
-    if (!(o instanceof final PointerKeyImpl<?> other)) {
+    if (!(o instanceof PointerKeyImpl<?>(String reference1, Class<?> type1))) {
       return false;
     }
 
-    return this.reference.equals(other.reference) && this.type.equals(other.type);
+    return this.reference.equals(reference1) && this.type.equals(type1);
   }
-
 }
